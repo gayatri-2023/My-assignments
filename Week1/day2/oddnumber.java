51f147136a4c36f673f18002f3e84e49b44f05af
gayatri-2023 <133882330+gayatri-2023@users.noreply.github.com>
2023-05-20 17:48:37 +0530

    Add files via upload
    Day 2 Assignment

---
+++ Week1/day2/oddnumber.java
@@ -0,0 +1,21 @@
+package Week1.day2;
+
+public class oddnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		int num = 37;
+		
+		for (num=0; num<=37;num++)
+		{
+			if(num%2!=0) {
+				System.out.println("odd number: "+num);}
+				
+			
+		}
+	}
+
+	}
