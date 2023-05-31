68ab963ba391a3c2f932035e67d7c86d20c67b75
gayatri-2023 <133882330+gayatri-2023@users.noreply.github.com>
2023-05-31 23:25:10 +0530

    Add files via upload

---
+++ Week3/day3/FindIntersectionUsingList.java
@@ -0,0 +1,42 @@
+package Week3.day3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindIntersectionUsingList {
+	
+	public static void main(String[] args) {
+		
+		int[] a= {3,2,11,4,6,7};
+		int[] b= {1,2,8,4,9,7};
+		
+		List<Integer> list1= new ArrayList<Integer>();
+		List<Integer> list2= new ArrayList<Integer>();
+		
+		for (int i=0;i<a.length;i++)
+		{
+		list1.add(a[i])	;
+		System.out.println(list1);
+		}
+		
+		
+		for (int j=0;j<b.length;j++)
+		{
+			list2.add(b[j])	;
+			System.out.println(list2);	
+		}
+		
+		
+		for (int i=0;i<list1.size();i++) {
+			
+			for (int j=0;j<list2.size();j++) {
+				
+			if (list1.get(i)== list2.get(j))
+			{ 
+				System.out.println("The matching number is "+ ""+ list1.get(i));
+			}
+			}
+		}
+	}
+
+}
